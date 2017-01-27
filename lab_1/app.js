d615c7c309029e52b03c8bf218fb3a18ba0f41c3
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-01-26 20:01:27 -0800

    lab_1

--- lab_1/app.js
+++ lab_1/app.js
@@ -1,17 +1,22 @@
-var answerTwo = prompt('Do you want to code?');
-var userAnswer = ('Yes', 'yes', 'y')
+// var answerTwo = prompt('Do you want to code?');
+// var userAnswer = ('Yes', 'yes', 'y')
+//
+// if(answerTwo === userAnswer) {
+//   console.log('Great let\'s start!');
+// } else {
+//   console.log('Maybe next time');
+// }
+//
+//
+var food = prompt('What type of food would you like to eat?'); // prompting the user with a string message.
+var foodTypes = ['pizza','tacos','burgers']; // creating an array and asigning it a value
+// foodTypes.indexOf('pizza'); //
+// foodTypes.indexOf('tacos');
+// foodTypes.indexOf('burgers');
+console.log('You\'re answer is ' + food);
+console.log(foodTypes.indexOf(food))
 
-if(answerTwo === userAnswer) {
-  console.log('Great let\'s start!');
-} else {
-  console.log('Maybe next time');
-}
-
-
-var food = prompt('What type of food would you like to eat?');
-var foodTypes = ['pizza', 'burgers', 'tacos'];
-
-if(food === foodTypes) {
+if(food.indexOf(foodTypes)) {
   alert('Sounds delicious!');
 } else {
   alert('You\'re disgusting');
