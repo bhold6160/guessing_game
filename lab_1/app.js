62b8a0398752e4042ef4a1f8b5c756e45223e197
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-01-26 20:00:54 -0800

    lab_1

---
+++ lab_1/app.js
@@ -0,0 +1,18 @@
+var answerTwo = prompt('Do you want to code?');
+var userAnswer = ('Yes', 'yes', 'y')
+
+if(answerTwo === userAnswer) {
+  console.log('Great let\'s start!');
+} else {
+  console.log('Maybe next time');
+}
+
+
+var food = prompt('What type of food would you like to eat?');
+var foodTypes = ['pizza', 'burgers', 'tacos'];
+
+if(food === foodTypes) {
+  alert('Sounds delicious!');
+} else {
+  alert('You\'re disgusting');
+}
