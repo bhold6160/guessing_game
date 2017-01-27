0c563d87c9ef5cff80b3ad43602e2fdf298ef435
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-01-26 21:03:05 -0800

    lab_1

--- lab_1/app.js
+++ lab_1/app.js
@@ -1,3 +1,18 @@
+//   var userName = prompt('What\'s your name?');
+// alert('Hello ' + userName + '. Thanks for coming to site!');
+//   // We want to ask the user a question,
+//   // take their answer as input; stored in a var
+//   // Validate that their answer matches my answer
+//   // Depending on true or false, provide feedback
+//
+//  var answerOne = prompt('What state was I born in?');
+//  var myAnswerOne = 'Washington';
+//  if(answerOne === myAnswerOne) {
+//    alert('You got it right. I was born in Washington');
+//  } else {
+//    alert('Sorry, that\'s wrong. I was born in Washington');
+//  }
+
 // var answerTwo = prompt('Do you want to code?');
 // var userAnswer = ('Yes', 'yes', 'y')
 //
@@ -10,14 +25,11 @@
 //
 var food = prompt('What type of food would you like to eat?'); // prompting the user with a string message.
 var foodTypes = ['pizza','tacos','burgers']; // creating an array and asigning it a value
-// foodTypes.indexOf('pizza'); //
-// foodTypes.indexOf('tacos');
-// foodTypes.indexOf('burgers');
-console.log('You\'re answer is ' + food);
-console.log(foodTypes.indexOf(food))
+console.log('You\'re answer is ' + food); //console logging the users answer to the prompted question
+console.log(foodTypes.indexOf(food)); //console logging where in the array the users answer is found or not found
 
-if(food.indexOf(foodTypes)) {
+if(foodTypes.indexOf(food) > -1) {
   alert('Sounds delicious!');
-} else {
+} else if(foodTypes.indexOf(food) === -1){
   alert('You\'re disgusting');
 }
