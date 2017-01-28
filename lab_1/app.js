b908b3781da40e5d9484107482d468457e3e84c7
bhold6160 <bhold6160@gmail.comgit config --listgit config --global user.email bhold6160@gmail.com>
2017-01-27 20:09:57 -0800

    lab_1

--- lab_1/app.js
+++ lab_1/app.js
@@ -1,35 +1,47 @@
 //   var userName = prompt('What\'s your name?');
 // alert('Hello ' + userName + '. Thanks for coming to site!');
+// console.log(userName)
 //   // We want to ask the user a question,
 //   // take their answer as input; stored in a var
 //   // Validate that their answer matches my answer
 //   // Depending on true or false, provide feedback
 //
 //  var answerOne = prompt('What state was I born in?');
-//  var myAnswerOne = 'Washington';
+//  var myAnswerOne = ('Washington', 'washington');
+//  console.log(answerOne)
 //  if(answerOne === myAnswerOne) {
 //    alert('You got it right. I was born in Washington');
 //  } else {
 //    alert('Sorry, that\'s wrong. I was born in Washington');
 //  }
-
+//
 // var answerTwo = prompt('Do you want to code?');
-// var userAnswer = ('Yes', 'yes', 'y')
+// var userAnswer = ('Yes' .toLowerCase());
+// console.log(answerTwo);
 //
 // if(answerTwo === userAnswer) {
-//   console.log('Great let\'s start!');
+//   alert('Great let\'s start!');
 // } else {
-//   console.log('Maybe next time');
+//   alert('Maybe next time');
 // }
-//
-//
-var food = prompt('What type of food would you like to eat?'); // prompting the user with a string message.
-var foodTypes = ['pizza','tacos','burgers']; // creating an array and asigning it a value
-console.log('You\'re answer is ' + food); //console logging the users answer to the prompted question
-console.log(foodTypes.indexOf(food)); //console logging where in the array the users answer is found or not found
 
-if(foodTypes.indexOf(food) > -1) {
-  alert('Sounds delicious!');
-} else if(foodTypes.indexOf(food) === -1){
-  alert('You\'re disgusting');
+var location = prompt('Do you live in Seattle?');
+var localAnswer = ('yes' .toUpperCase());
+
+if(location === localAnswer) {
+  alert('You\'re a Seattleite!');
+} else {
+  alert('Welcome to our city!');
 }
+
+// var food = prompt('What type of food would you like to eat?'); // prompting the user with a string message.
+// var foodTypes = ['pizza','tacos','burgers']; // creating an array and asigning it a value
+//
+// console.log('You\'re answer is ' + food); //console logging the users answer to the prompted question
+// console.log(foodTypes.indexOf(food)); //console logging where in the array the users answer is found or not found
+//
+// if(foodTypes.indexOf(food) > -1) {
+//   alert('Sounds delicious!');
+// } else if(foodTypes.indexOf(food) <= -1) {
+//   alert('Disgusting...');
+// }
